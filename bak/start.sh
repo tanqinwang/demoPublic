e3e1685eea69f023f769bc1b1adc0086fef0512b
szczepanek <tanqinwang@gmail.com>
2024-07-18 09:40:01 +0000

    update

---
+++ bak/start.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# run
+mkdir /var/www
+# chown -R www-data:www-data /var/www
+# service nginx restart
+# if [ ! -d '/run/php' ]; then
+#     mkdir /run/php
+#     chmod -R 755 /run/php
+# fi
+# service php8.2-fpm restart
+# chmod +x /run/php/php8.2-fpm.sock
+
+tail -f /dev/null
